25ba7bd892899ba9bc6851ec584b837aeba04f79
永橋恵美 <nagahashimegumi@nagahashieminoMacBook-Pro.local>
2022-03-11 06:26:55 +0900

    create_top #3

---
+++ app/javascript/css/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: ['./app/views/**/*.html.erb',
+  './app/helpers/**/*.rb',
+  './app/javascript/**/*.js',],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
